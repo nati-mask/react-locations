7c777ec715e5c88fc4445a6ca25769282f34ae2f
Nati Mask <natimask@gmail.com>
2018-05-30 23:29:20 +0300

    location list designed

--- src/components/nav/NavContainer.jsx
+++ src/components/nav/NavContainer.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 const { setPage } = require('../../actions');
 
+require('./NavContainer.less');
+
 class NavContainer extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
         return (
-            <div>
-                <button onClick={this.props.navClick.bind(this, "LocationsList")}>Locations</button>
-                <button onClick={this.props.navClick.bind(this, "CategoriesList")}>Categories</button>
+            <div className="nav">
+                <i className="material-icons nav-action" onClick={this.props.navClick.bind(this, "LocationsList")}>location_on</i>
+                <i className="material-icons nav-action" onClick={this.props.navClick.bind(this, "CategoriesList")}>category</i>
             </div>
         )
     }
